2c08951da88e12e11d70cebf7dd006e0c5211537
Helen L <htslin103@gmail.com>
2022-10-05 20:11:28 -0600

    Added all files

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RandomNumberGenerator
+{
+    using RandomNumberGenerator.Classes;
+    using System.Diagnostics;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newArray = Enumerable.Range(1, 10000).ToArray();
+            var randomList = RandNumberGenerator.CreateRandomList(newArray);
+            RandNumberGenerator.PrintList(randomList);
+        }
+    }
+}
